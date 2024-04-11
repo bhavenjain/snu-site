0c604ca6125481c70b6be1e8ce6a6e11d176c8d3
= <bj995@snu.edu.in>
2024-04-12 00:44:05 +0530

    changes

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -66,6 +66,17 @@ const Admin = () => {
           theme: "colored",
         });
         setCategoryName("");
+      } else {
+        toast.error(response?.data?.message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
       }
     } catch (err) {
       toast.error(response?.data?.message, {
