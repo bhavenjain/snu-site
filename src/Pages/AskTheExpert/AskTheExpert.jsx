ad118db5b5a48c6925126725d90f82307fb841a7
= <bj995@snu.edu.in>
2024-04-11 21:45:07 +0530

    api connection started

--- src/Pages/AskTheExpert/AskTheExpert.jsx
+++ src/Pages/AskTheExpert/AskTheExpert.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 // Styles
 import styles from "./AskTheExpert.module.css";
@@ -18,6 +18,24 @@ const AskTheExpert = () => {
   // Modal Functions
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const getCategories = async () => {
+    const url = process.env.VITE_API_HOST + "/web/fetch/category/faq"
+    const data = await axios.get(url, {
+      header: {},
+      params: {}
+    })
+    return data?.data
+  }
+
+  useEffect(() => {
+    try {
+      const categories = getCategories()
+      console.log("............data", categories)
+    } catch(err) {
+      console.log(err)
+    }
+  })
 
   return (
     <div className={styles.web_container}>
@@ -42,18 +60,6 @@ const AskTheExpert = () => {
       <div className={styles.question_container}>
         <Accordion defaultActiveKey="0">
           <Accordion.Item eventKey="0">
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
             <Accordion.Header>Accordion Item #1</Accordion.Header>
             <Accordion.Body>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
