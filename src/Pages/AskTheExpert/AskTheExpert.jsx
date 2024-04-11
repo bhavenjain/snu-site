d460d009ab7ff0d14021b32068785b57aa8daa8b
= <bj995@snu.edu.in>
2024-04-11 22:32:22 +0530

    faq pages

--- src/Pages/AskTheExpert/AskTheExpert.jsx
+++ src/Pages/AskTheExpert/AskTheExpert.jsx
@@ -14,6 +14,7 @@ import OpenModal from "../../Components/OpenModal/OpenModal";
 const AskTheExpert = () => {
   // States
   const [show, setShow] = useState(false);
+  const [allFaqs, setAllFaqs] = useState([]);
   const [allCategories, setAllCAtegories] = useState([]);
   const [categoryText, setCategoryText] = useState("Select Category");
 
@@ -21,6 +22,7 @@ const AskTheExpert = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  // Function to call api to get all the categories
   const getCategories = async () => {
     const url = "http://127.0.0.1:8000" + "/web/fetch/category/faq";
     const response = await axios.get(url, {
@@ -30,6 +32,23 @@ const AskTheExpert = () => {
       params: {},
     });
     setAllCAtegories(response?.data?.data);
+    if (response?.data?.data?.length > 0)
+      setCategoryText(response?.data?.data?.at(0));
+
+    return response?.data?.data;
+  };
+
+  // Function to call api to get all faqs
+  const getFaqs = async () => {
+    const url = "http://127.0.0.1:8000" + `/web/faq/fetch/all/${categoryText}`;
+    const response = await axios.get(url, {
+      header: {
+        Authorization: "",
+      },
+      params: {},
+    });
+    setAllFaqs(response?.data?.data);
+
     return response?.data?.data;
   };
 
@@ -41,12 +60,28 @@ const AskTheExpert = () => {
     }
   }, []);
 
+  /**
+   * Check if category text has been changed
+   * and call faqs according to that
+   */
+  useEffect(() => {
+    if (categoryText !== "Select Category") {
+      try {
+        getFaqs();
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, [categoryText]);
+
   return (
     <div className={styles.web_container}>
       <h1 className={styles.heading}>Frequently Asked Questions</h1>
       <p>
         Which of the categories below best describes the nature of your concern?
       </p>
+
+      {/* Category Dropdown */}
       {allCategories?.length > 0 ? (
         <div className={styles.category_dropdown}>
           <Dropdown>
@@ -72,33 +107,25 @@ const AskTheExpert = () => {
         <></>
       )}
 
+      {/* All Faqs */}
       <div className={styles.question_container}>
         <Accordion defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
+          {allFaqs?.length > 0 ? (
+            allFaqs?.map((item, key) => {
+              return (
+                <Accordion.Item key={key} eventKey={key}>
+                  <Accordion.Header>{item?.question}</Accordion.Header>
+                  <Accordion.Body>
+                    {item?.answer}
+                  </Accordion.Body>
+                </Accordion.Item>
+              );
+            })
+          ) : (
+            <></>
+          )}
         </Accordion>
       </div>
-
-      {show ? (
-        <OpenModal
-          heading="Lorem Ipsum"
-          body="Lorem Ipsum tipsum chipsum"
-          show={show}
-          handleClose={handleClose}
-        />
-      ) : (
-        <></>
-      )}
     </div>
   );
 };
