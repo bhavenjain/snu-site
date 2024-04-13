df05d698ad904cfafd109b5500d29ac035595781
= <bj995@snu.edu.in>
2024-04-13 16:17:43 +0530

    changes

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -11,6 +11,12 @@ import styles from "./Register.module.css";
 import Loader from "../../Components/Loader/Loader";
 import LoginHeader from "../../Components/LoginHeader/LoginHeader";
 
+const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
+
 const Register = () => {
   const navigate = useNavigate();
 
@@ -69,6 +75,18 @@ const Register = () => {
       return;
     } else if(password?.length < 8) {
       toast.error("Password must be of 8 characters", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      return;
+    } else if(!validateEmail(email)) {
+      toast.error("Please enter a valid email", {
         position: "top-right",
         autoClose: 5000,
         hideProgressBar: false,
