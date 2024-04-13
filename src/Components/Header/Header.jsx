6556f4669669938a476be868029f82cf648c43e0
= <bj995@snu.edu.in>
2024-04-13 14:57:36 +0530

    changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Cookies from 'js-cookie'
 
 // Styles
 import styles from "./Header.module.css"
 
 const Header = () => {
+  const user = Cookies.get("user") ? JSON.parse(Cookies.get("user")) : {}
   return (
     <div className={styles.header}>
-        <b>Hello, Username</b>
+        <b>Hello, {user?.name}</b>
     </div>
   )
 }
