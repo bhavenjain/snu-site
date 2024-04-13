68a7b3e2294f7b48ff6b123c1ee268d4b620240d
= <bj995@snu.edu.in>
2024-04-13 15:22:13 +0530

    changes

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -126,16 +126,6 @@ const Login = () => {
       })
       .catch((err) => {
         setLoader(false);
-        toast.error("Some error has occured", {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "colored",
-        });
       });
   }, []);
 
