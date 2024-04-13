ffb71eba283c610cbbc22436df1e008e627bbbc9
= <bj995@snu.edu.in>
2024-04-13 14:30:33 +0530

    changes

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,32 +1,36 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios"
+import axios from "axios";
 import Cookies from "js-cookie";
+import { ToastContainer, toast } from "react-toastify";
 
 // Styles
 import styles from "./Login.module.css";
+
+// Components
 import Loader from "../../Components/Loader/Loader";
 import LoginHeader from "../../Components/LoginHeader/LoginHeader";
 
 const Login = () => {
-  const [email, setEmail] = useState("")
+  const [email, setEmail] = useState("");
   const [loader, setLoader] = useState(false);
-  const [password, setPassword] = useState("")
+  const [password, setPassword] = useState("");
 
   // Utility functions
   /**
    * Function to handle email input
    */
   const handleEmail = (e) => {
-    setEmail(e.target.value)
-  }
+    setEmail(e.target.value);
+  };
 
   /**
    * Function to handle password input
    */
   const handlePassword = (e) => {
-    setPassword(e.target.value)
-  }
+    setPassword(e.target.value);
+  };
 
+  // Get auth to check for user
   const getAuth = async () => {
     const url = "http://127.0.0.1:8000" + "/web/fetch/user";
     const user = await axios.get(url, {
@@ -37,15 +41,58 @@ const Login = () => {
     return user;
   };
 
+  // handle submit for login
+  const handleSubmit = async () => {
+    if(email?.length ===0 || password?.length === 0) {
+      toast.error("Please enter all the details", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      return;
+    }
+
+    try {
+      const url = "http://127.0.0.1:8000" + "web/login";
+      const user = await axios.post(url, {
+        email: email,
+        password: password
+      }, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log("........user", user)
+
+    } catch (err) {
+      toast.error("There was some error. Please, try again.", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    }
+  }
+
+  // useEffect for the initial Apis
   useEffect(() => {
     getAuth()
       ?.then((response) => {
         if (response) {
-          console.log(".........", response?.data?.data?.role, response?.data?.data)
           if (response?.status === 200) {
             setLoader(false);
-            if(response?.data?.data?.role === "admin") {
-              navigate("/admin/add-details/portal");
+            if (response?.data?.data?.role === "admin") {
+              window.location.href = "/admin/add-details/portal";
             } else {
               window.location.href = "/dashboard/web-bridge-portal";
             }
@@ -59,7 +106,9 @@ const Login = () => {
       });
   }, []);
 
-  return loader ? <Loader /> :  (
+  return loader ? (
+    <Loader />
+  ) : (
     <div className={`${styles.container}`}>
       {/* Login header logos */}
       <LoginHeader />
@@ -73,12 +122,38 @@ const Login = () => {
           to support your board journey
         </h2>
         <div className={styles.login_form}>
-          <input type={"email"} placeholder="Enter Email" value={email} onChange={handleEmail} />
-          <input type={"password"} placeholder="Password" value={password} onChange={handlePassword} />
-          <a href={"#"} className={styles.forgot}>Forgot Password?</a>
-          <a href="#" className={styles.login_button}>Login</a>
+          <input
+            type={"email"}
+            placeholder="Enter Email"
+            value={email}
+            onChange={handleEmail}
+          />
+          <input
+            type={"password"}
+            placeholder="Password"
+            value={password}
+            onChange={handlePassword}
+          />
+          <a href={"#"} className={styles.forgot}>
+            Forgot Password?
+          </a>
+          <div onClick={handleSubmit} className={styles.login_button}>
+            Login
+          </div>
         </div>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   );
 };
