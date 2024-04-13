a329093b4dee1337b6efe63540ff0c5136c4ead9
= <bj995@snu.edu.in>
2024-04-13 13:56:47 +0530

    changes

--- src/Components/ProtectedRoutes/ProtectedRoutes.jsx
+++ src/Components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 import { Outlet, useNavigate } from "react-router-dom";
