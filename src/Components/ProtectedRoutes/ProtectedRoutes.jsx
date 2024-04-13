a7ccd40d082e75d3efc2a4490c989ffbeaa85f91
= <bj995@snu.edu.in>
2024-04-13 13:54:47 +0530

    changes

--- src/Components/ProtectedRoutes/ProtectedRoutes.jsx
+++ src/Components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import Cookies from "js-cookie";
 import { Outlet, useNavigate } from "react-router-dom";
 
 // Components
@@ -8,29 +9,34 @@ import Loader from "../Loader/Loader";
 const ProtectedRoutes = () => {
   const navigate = useNavigate();
 
+  // States
+  const [loader, setLoader] = useState(true)
+
   const getAuth = async () => {
     const url = "http://127.0.0.1:8000" + "/web/fetch/user";
     const user = await axios.get(url, {
       headers: {
-        "Authorization" : ""
+        "Authorization" : Cookies.get("token")
       },
     });
-
-    console.log("................", user)
-    return user?.data
+    return user
   };
 
   useEffect(() => {
     getAuth()?.then(response => {
       if(response) {
-        console.log("......user inside", response)
+        if(response?.status === 200) {
+          setLoader(false)
+        } else {
+          navigate("/login")
+        }
       }
     }).catch(err => {
       console.log("......error", err)
     })
   }, []);
 
-  return <Outlet />;
+  return loader ? <Loader /> : <Outlet />;
 };
 
 export default ProtectedRoutes;
