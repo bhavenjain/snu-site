5ab39499e3a6abe4549f189eeeb58de0215acff2
= <bj995@snu.edu.in>
2024-04-20 11:51:30 +0530

    Ecosystem file added for pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,10 +3,14 @@ module.exports = {
       {
         name: `web-bridge-snu`,
         script: "serve",
+        autorestart: true,
         env: {
-          PM2_SERVE_PATH: "./dist",
+          // PM2_SERVE_PATH: "./dist",
           PM2_SERVE_PORT: 3000,
           PM2_SERVE_SPA: "true",
+          NODE_ENV: 'production',
+        },
+        env_production: {
           NODE_ENV: 'production',
         },
       },
