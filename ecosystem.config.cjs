3ebca868dc9212084e38fc251ecc505cd64eaea5
= <bj995@snu.edu.in>
2024-04-20 14:45:10 +0530

    changes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,16 +3,6 @@ module.exports = {
       {
         name: `web-bridge-snu`,
         script: "npm run dev -- --host",
-        // autorestart: true,
-        env: {
-          // PM2_SERVE_PATH: "./dist",
-          // PM2_SERVE_PORT: 3000,
-          // PM2_SERVE_SPA: "true",
-          NODE_ENV: 'production',
-        },
-        env_production: {
-          NODE_ENV: 'production',
-        },
       },
     ],
   };
