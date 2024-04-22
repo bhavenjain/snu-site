3bccbd8236f6ef26219dfe81cde7f351c59d3fbe
= <bj995@snu.edu.in>
2024-04-22 21:49:12 +0530

    changes

--- src/Components/OpenModal/OpenModal.jsx
+++ src/Components/OpenModal/OpenModal.jsx
@@ -3,6 +3,37 @@ import React from "react";
 // Bootstrap
 import Modal from "react-bootstrap/Modal";
 
+function parseText(text) {
+  // Split the text by line breaks
+  const lines = text.split('\n');
+
+  // Map each line to include paragraph and anchor tags
+  const parsedLines = lines.map(line => {
+    // Split the line by spaces to find links
+    const words = line.split(' ');
+
+    // Map each word to include anchor tags for links
+    const parsedWords = words.map(word => {
+      if (word.startsWith('http://') || word.startsWith('https://')) {
+        return `<a href="${word}">${word}</a>`;
+      }
+      return word;
+    });
+
+    // Join the words back together with spaces
+    const parsedLine = parsedWords.join(' ');
+
+    // Wrap the line with paragraph tags
+    return `<p>${parsedLine}</p>`;
+  });
+
+  // Join the lines back together with <br> tags
+  const parsedText = parsedLines.join('<br>');
+
+  // Return the parsed text
+  return { __html: parsedText };
+}
+
 const OpenModal = ({ heading, show, handleClose, body }) => {
   return (
     <Modal
@@ -15,7 +46,7 @@ const OpenModal = ({ heading, show, handleClose, body }) => {
       <Modal.Header closeButton>
         <Modal.Title>{heading}</Modal.Title>
       </Modal.Header>
-      <Modal.Body dangerouslySetInnerHTML={{ __html: body.replace(/\n/g, '<br>') }} />
+      <Modal.Body dangerouslySetInnerHTML={parseText(body)} />
     </Modal>
   );
 };
