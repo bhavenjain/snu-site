624d526b1f8027a19e5534d369d6ac2efa7ad84c
= <bj995@snu.edu.in>
2024-04-22 22:25:14 +0530

    changes

--- src/Components/OpenModal/OpenModal.jsx
+++ src/Components/OpenModal/OpenModal.jsx
@@ -3,32 +3,43 @@ import React from "react";
 // Bootstrap
 import Modal from "react-bootstrap/Modal";
 
+// Styles
+import styles from "./OpenModal.module.css";
+
 function parseText(text) {
   // Split the text by line breaks
-  const lines = text.split('\n');
+  const lines = text.split("\n");
 
   // Map each line to include paragraph and anchor tags
-  const parsedLines = lines.map(line => {
+  let parsedLines = lines.map((line) => {
     // Split the line by spaces to find links
-    const words = line.split(' ');
+    const words = line.split(" ");
 
     // Map each word to include anchor tags for links
-    const parsedWords = words.map(word => {
-      if (word.startsWith('http://') || word.startsWith('https://')) {
-        return `<a href="${word}">${word}</a>`;
-      }
-      return word;
+    const parsedWords = words.map((word) => {
+      return word.replace(
+        /(\b(?:https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|]|www\.[A-Z0-9.-]+\.[A-Z]{2,})/gi,
+        (match, link) => {
+          return `<a href="${
+            link?.startsWith("www") ? "https://" + link : link
+          }" target="__blank" rel="noopener noreferrer">${link}</a>`;
+        }
+      );
     });
 
     // Join the words back together with spaces
-    const parsedLine = parsedWords.join(' ');
+    let parsedLine = parsedWords.join(" ");
+    if (parsedLine?.length > 0) {
+      return `<p>${parsedLine}</p>`;
+    }
+  });
 
-    // Wrap the line with paragraph tags
-    return `<p>${parsedLine}</p>`;
+  parsedLines = parsedLines?.filter((item) => {
+    return item?.length > 0;
   });
 
   // Join the lines back together with <br> tags
-  const parsedText = parsedLines.join('<br>');
+  const parsedText = parsedLines.join("<br>");
 
   // Return the parsed text
   return { __html: parsedText };
@@ -46,7 +57,12 @@ const OpenModal = ({ heading, show, handleClose, body }) => {
       <Modal.Header closeButton>
         <Modal.Title>{heading}</Modal.Title>
       </Modal.Header>
-      <Modal.Body dangerouslySetInnerHTML={parseText(body)} />
+      <Modal.Body>
+        <div
+          className={styles.body}
+          dangerouslySetInnerHTML={parseText(body)}
+        />
+      </Modal.Body>
     </Modal>
   );
 };
