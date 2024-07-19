8689ba523f4197502b7d4d9b68eb1caef1974f15
= <bj995@snu.edu.in>
2024-07-20 01:44:32 +0530

    About us page content updated

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -45,9 +45,6 @@ function parseText(text) {
 }
 
 const About = () => {
-  // States
-  const [heading, setHeading] = useState("");
-
   return (
     <>
       <img src="/Banner_with_text.svg" alt="banner" />
@@ -56,8 +53,85 @@ const About = () => {
         <h3>About</h3>
 
         <div className={styles.announcements_body}>
-          <h5>{heading}</h5>
-          <p>About the Web bridge Platform</p>
+          <div>
+            <h5>The Program</h5>
+            <p>
+              The program aims to provide women leaders with the skills and
+              knowledge they need to successfully transition from executive
+              positions to corporate board roles.
+            </p>
+
+            <p>
+              The program also offers women leaders on the Board a chance to
+              enhance their skills and optimize their performance as directors ,
+              both for their own enterprises and for other companies.
+            </p>
+
+            <p>The program will enable participants to</p>
+            <ul>
+              <li>
+                understand roles and responsibilities of corporate directors and
+                board committees
+              </li>
+              <li>
+                match their skills and talents to the specific needs of the
+                board
+              </li>
+              <li>
+                carry out board tasks and engage in collaborative decision
+                making
+              </li>
+              <li>
+                add value to an organization as overseers of its leadership
+                ,strategy and overseers of performances
+              </li>
+              <li>
+                navigate the challenges and put laws and perspectives into
+                practice
+              </li>
+              <li>
+                manage personal reputation as a board director and mitigate
+                their own risks
+              </li>
+              <li>
+                learn from global experts on how to read a corporate board and
+                take decisions in a multi-cultural environment
+              </li>
+            </ul>
+
+            <h5>WEB Bridge</h5>
+            <p>
+              This is a portal that will guide you on any professional dilemma
+              or questions that you may face while discharging duties as a
+              director. The portal contains a library of materials relevant to
+              Board practices and applicable laws and relevant preparatory tools
+              for qualifying for the Independent Director’s examination
+              conducted by the Indian Institute of Corporate Affairs (IICA). You
+              will be able to learn at your own pace. A test preparedness quiz
+              is provided with which you can test yourself to align your
+              preparation strategy appropriately.
+            </p>
+            <p>
+              The Ask the Expert feature allows you to pose questions to our
+              experts which they will answer through the portal. All your
+              questions will be dealt with while always maintaining the highest
+              levels of confidentiality.
+            </p>
+
+            <p>
+              Over the course of the calendar year, different learning
+              opportunities will be offered. These include:
+            </p>
+
+            <ul>
+              <li>Masterclasses</li>
+              <li>
+                1x1 learning clinics (in finance, governance and similar areas)
+              </li>
+              <li>Updates on legal/policy development</li>
+              <li>And much more</li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
